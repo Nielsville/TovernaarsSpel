f32366e84350fd9ec5096d7f5c8985589a6c1467
Unknown <Nielsscholten2nd@gmail.com>
2018-11-26 15:00:45 +0100

    Added project

---
+++ TovernaarsKamer/Assets/Scripts/MainManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour{
+
+
+    public MedeklinkerRenderer LijstMedeklinkers;
+    // Lijst met alle woorden.
+    public List<string> Woorden = new List<string>();
+
+    // Lijst met alle letters van een woord.
+    private List<string> Letters = new List<string>();
+
+    // String met woord dat in het spel wordt gebruikt.
+    public string Woord;
+
+    // String voor goede Medeklinker
+    private string GoedeMedeklinker;
+
+    // GameObject waar word komt te staan
+    [SerializeField] public GameObject FullWord;
+
+    // Gameobject waar aantal punten van het woord komt te Staan.
+    
+	// Use this for initialization
+	void Start () {
+        // Pakt random woord uit lijst.
+        Woord = Woorden[Random.Range(0,Woorden.Count - 1)];
+        //FullWord.GetComponent<TextMesh>().text = Woord;
+
+        // Pakt letters van een woord.
+        LijstMedeklinkers.MedeklinkersOptellen();
+        GoedeMedeklinker = LijstMedeklinkers.medeklinkers;
+        FullWord.GetComponent<TextMesh>().text = Woord;
+        // Pakt random extra letters.
+
+        // (Optioneel) Zet random letters op kasten en vliegende boeken.
+
+        // Verdeeld de letters van de woord over de hele scherm.
+        // Twee opties Mogelijk
+        // 1. Random over het hele scherm.
+        // 2. Op Bepaalde plekken op hett scherm zoals de tafel of op planken.
+        // Zorg ook dat de letters een kleine afstand van elkaar hebben.
+
+        // Verdeeld de randowm letters door het scherm heen.
+
+        // (Optioneel) Plaatje object van woord komt ervoorschijn.
+
+        // (Optioneel) Toverneer legt de spelregels uit aan de kinderen.
+    }
+	
+    // Als op een letter is gegooit dan word er gekeken of het woord bij het plaatje past.
+
+    // Zo Ja dan zweeft de letter naar een scherm met lege puntjes.
+
+    // Zo nee dan gebeurd er niets.
+
+    // Als de letter naar tevoren is gekomen Dan komt er een victory screen.
+
+    // (Optioneel) Maak het mogelijk om kasten en dueren open te maken.
+
+    // (Optioneel) Zet geluid in het spel voor levendigheid.
+
+    // (Optioneel) Laat deze code 5 keer herhalen.
+
+	void Update () {
+		//(Optioneel) Laat vliegende boeken over het scherm bewegen.
+	}
+}
---
+++ TovernaarsKamer/Assets/Scripts/MedeklinkerRenderer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedeklinkerRenderer : MonoBehaviour {
+    [SerializeField] public MainManager Controller;
+    public string medeklinkers;
+    public void MedeklinkersOptellen()
+    {
+        for (int i = 0; i < Controller.Woord.Length - 1; i++)
+        {
+            switch (Controller.Woord[i])
+            {
+                case 'A':
+                    medeklinkers += 'a';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'a':
+                    medeklinkers += 'a';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'E':
+                    medeklinkers += 'e';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'e':
+                    medeklinkers += 'e';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'I':
+                    medeklinkers += 'i';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'i':
+                    medeklinkers += 'i';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'O':
+                    medeklinkers += 'o';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'o':
+                    medeklinkers += 'o';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'U':
+                    medeklinkers += 'u';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+                case 'u':
+                    medeklinkers += 'u';
+                    Controller.Woord.Substring(i);
+                    i = i - 1;
+                    break;
+            }
+            if (medeklinkers == "I" || medeklinkers == "I")
+            {
+                switch (Controller.Woord[i])
+                {
+                    case 'j':
+                        medeklinkers += 'j';
+                        Controller.Woord.Substring(i);
+                        i = i - 1;
+                        break;
+                }
+            }
+        }
+    }
+    public List<string> FouteMedeklinkers = new List<string>();
+    public void ZetKlinkers()
+    {
+
+    }
+
+}
